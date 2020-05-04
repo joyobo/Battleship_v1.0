c98587aea4bb8b9906fe3a8df976d81cd1db7fe5
joyobo <56425212+joyobo@users.noreply.github.com>
2020-05-05 03:06:32 +0800

    Create battleship.py

---
+++ battleship.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix.label import Label
+# uix is a folder in kivy. label is a class 
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import BooleanProperty, ObjectProperty, NumericProperty, StringProperty
+from kivy.uix.popup import Popup
+from kivy.clock import Clock
+from kivy.uix.image import Image
+from kivy.graphics import *
+import random
+import copy
